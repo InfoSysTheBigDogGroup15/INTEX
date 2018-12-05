b28600bee3091f92eac1a7103d4d03920df13d2e
Hoyong Jung <jhoyong57@gmail.com>
2018-12-04 23:42:08 -0700

    hoyong added DAL

---
+++ INTEX/DAL/NorthwestContext.cs
@@ -0,0 +1,29 @@
+﻿using INTEX.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.DAL
+{
+    public class NorthwestContext : DbContext
+    {
+        public NorthwestContext() : base("NorthwestContext")
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Component> Component { get; set; }
+        public DbSet<Discount> Discount { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Sample> Sample { get; set; }
+        public DbSet<Status> Status { get; set; }
+        public DbSet<Test> Test { get; set; }
+        public DbSet<Test_Record> Test_Record { get; set; }
+        public DbSet<Test_Tube> Test_Tube { get; set; }
+        public DbSet<Work_Order> Work_Order { get; set; }
+    }
+}
