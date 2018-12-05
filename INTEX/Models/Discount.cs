d38d61649e584281ade81541d0d8231972d8e2e0
Grant Valentine <grantsvalentine@gmail.com>
2018-12-05 10:13:55 -0700

    The rest of the Models

---
+++ INTEX/Models/Discount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.Models
+{
+    [Table("Discount")]
+    public class Discount
+    {
+        [Key]
+        public int discountID { get; set; }
+
+        [DisplayName("Description")]
+        [Required(ErrorMessage = "Please enter the description")]
+        public String description { get; set; }
+
+        [DisplayName("Discount Percentage")]
+        [Required(ErrorMessage = "Please enter the discount percentage")]
+        public double percentDiscount { get; set; }
+
+    }
+}
---
+++ INTEX/Models/Test_Tube.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.Models
+{
+    [Table("Test_Tube")]
+    public class Test_Tube
+    {
+        [Key]
+        public int testTubeID { get; set; }
+
+        [DisplayName("Concentration")]
+        [Required(ErrorMessage = "Please enter the concentration")]
+        public String concentration { get; set; }
+
+        public int? testID { get; set; }
+        public virtual Test Test { get; set; }
+    }
+}
