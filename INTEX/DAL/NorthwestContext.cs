31d0f5a540dc492303166c43ab12fd93261bff54
Hoyong Jung <jhoyong57@gmail.com>
2018-12-05 21:24:14 -0700

    Hoyong found some bugs

--- INTEX/DAL/NorthwestContext.cs
+++ INTEX/DAL/NorthwestContext.cs
@@ -15,7 +15,7 @@ namespace INTEX.DAL
 
         }
 
-        public DbSet<Client> Clients { get; set; }
+        public DbSet<Client> Client { get; set; }
         public DbSet<Component> Component { get; set; }
         public DbSet<Discount> Discount { get; set; }
         public DbSet<Employee> Employee { get; set; }
