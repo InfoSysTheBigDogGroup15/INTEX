62be006a14d414558a89e3653cc615d6d2d2d4e3
Hoyong Jung <jhoyong57@gmail.com>
2018-12-07 10:47:52 -0700

    Hoyong added about us page

--- INTEX/Controllers/HomeController.cs
+++ INTEX/Controllers/HomeController.cs
@@ -167,5 +167,9 @@ namespace INTEX.Controllers
         }
         //public ActionResult CreateClientUser(FormColle)
        
+        public ActionResult AboutUs ()
+        {
+            return View();
+        }
     }
 }
